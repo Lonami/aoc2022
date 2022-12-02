32162d9bce4938c180f37ba9c107508396b85f00
Lonami Exo <totufals@hotmail.com>
2022-12-02 18:03:23 +0100

    day02/pt2: Add solution

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -21,7 +21,7 @@ enum PlayResult {
 #[derive(Debug)]
 struct Play {
     opponet: Hand,
-    own: Hand,
+    expected: PlayResult,
 }
 
 impl Hand {
@@ -45,14 +45,40 @@ impl Hand {
 }
 
 impl PlayResult {
+    fn assume_hand(&self) -> Hand {
+        match *self {
+            PlayResult::Defeat => Hand::Rock,
+            PlayResult::Draw => Hand::Paper,
+            PlayResult::Victory => Hand::Scissors,
+        }
+    }
+
+    fn needed_hand(&self, other: Hand) -> Hand {
+        match (*self, other) {
+            (PlayResult::Draw, Hand::Rock) => Hand::Rock,
+            (PlayResult::Defeat, Hand::Paper) => Hand::Rock,
+            (PlayResult::Victory, Hand::Scissors) => Hand::Rock,
+            (PlayResult::Victory, Hand::Rock) => Hand::Paper,
+            (PlayResult::Draw, Hand::Paper) => Hand::Paper,
+            (PlayResult::Defeat, Hand::Scissors) => Hand::Paper,
+            (PlayResult::Defeat, Hand::Rock) => Hand::Scissors,
+            (PlayResult::Victory, Hand::Paper) => Hand::Scissors,
+            (PlayResult::Draw, Hand::Scissors) => Hand::Scissors,
+        }
+    }
+
     fn score(&self) -> i32 {
         *self as u8 as i32
     }
 }
 
 impl Play {
+    fn assumed_score(&self) -> i32 {
+        self.expected.assume_hand().score() + self.expected.assume_hand().beats(self.opponet).score()
+    }
+
     fn score(&self) -> i32 {
-        self.own.score() + self.own.beats(self.opponet).score()
+        self.expected.needed_hand(self.opponet).score() + self.expected.score()
     }
 }
 
@@ -65,16 +91,29 @@ fn parse_hand(chr: u8) -> Result<Hand> {
     }
 }
 
+fn parse_victory_result(chr: u8) -> Result<PlayResult> {
+    match chr {
+        b'X' => Ok(PlayResult::Defeat),
+        b'Y' => Ok(PlayResult::Draw),
+        b'Z' => Ok(PlayResult::Victory),
+        _ => Err("bad play result".into())
+    }
+}
+
 fn parse_input(input: &str) -> Result<Vec<Play>> {
     Ok(input
         .lines()
         .try_fold(Vec::new(), |mut plays, line| {
             plays.push(Play {
                 opponet: parse_hand(line.as_bytes()[0])?,
-                own: parse_hand(line.as_bytes()[2])?
+                expected: parse_victory_result(line.as_bytes()[2])?
             });
             Ok::<_, Error>(plays)
         })?)
+}
+
+fn get_assumed_plan_score(plan: &[Play]) -> i32 {
+    plan.iter().map(|play| play.assumed_score()).sum()
 }
 
 fn get_plan_score(plan: &[Play]) -> i32 {
@@ -83,7 +122,8 @@ fn get_plan_score(plan: &[Play]) -> i32 {
 
 fn main() -> Result<()> {
     let input = parse_input(&fs::read_to_string("inputs/02/1")?)?;
-    println!("Part 1 answer: {}", get_plan_score(&input));
+    println!("Part 1 answer: {}", get_assumed_plan_score(&input));
+    println!("Part 2 answer: {}", get_plan_score(&input));
     Ok(())
 }
 
@@ -94,7 +134,14 @@ mod tests {
     #[test]
     fn example1() -> Result<()> {
         let input = parse_input(&fs::read_to_string("inputs/02/1.test")?)?;
-        assert_eq!(get_plan_score(&input), 15);
+        assert_eq!(get_assumed_plan_score(&input), 15);
+        Ok(())
+    }
+
+    #[test]
+    fn example2() -> Result<()> {
+        let input = parse_input(&fs::read_to_string("inputs/02/1.test")?)?;
+        assert_eq!(get_plan_score(&input), 12);
         Ok(())
     }
 }
